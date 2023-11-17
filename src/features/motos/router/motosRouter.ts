bddafb94a30653206016a7d9db0c4014405114c3
Daniel Andreu <daniel1997ar@gmail.com>
2023-11-17 14:11:41 +0100

    Add deleteMoto method in motosController and router

--- src/features/motos/router/motosRouter.ts
+++ src/features/motos/router/motosRouter.ts
@@ -9,6 +9,7 @@ const motosController = new MotosController(motosRepository);
 
 motosRouter.get("/", motosController.getMotos);
 motosRouter.get("/:motoId", motosController.getMotoById);
+motosRouter.delete("/:motoId", motosController.deleteMoto);
 motosRouter.post("/", motosController.createMoto);
 
 export default motosRouter;
